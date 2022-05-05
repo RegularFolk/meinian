ba6606efb5293b59fd28e87fad4a76a8f4448e83
kevin <1980817350@qq.com>
2022-05-05 18:56:17 +0800

    修改了第五行代码，增加master

--- meinian_common/src/main/java/com/atguigu/util/ABC.java
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -2,6 +2,6 @@ package com.atguigu.util;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("111111111111111");
+        System.out.println("111111111111111master");
     }
 }
